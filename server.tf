17e061c1dadba723a2fb92d5ae7d1442f30be209
Venketgithub <venketesanm@gmail.com>
2024-09-27 14:54:46 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -13,6 +13,7 @@ region = "us-east-1"
 resource "aws_instance" "myawsserver" {
   ami = "ami-0e54eba7c51c234f6"
   instance_type = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.venket_ssh.id]
   key_name = "venket-keypair"
 
   tags = {
