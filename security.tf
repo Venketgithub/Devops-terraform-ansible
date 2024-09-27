606206fd1d39c1ff0fe1aaccc917d6a3647ee15c
Venketgithub <venketesanm@gmail.com>
2024-09-27 14:59:24 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group" "venket_ssh" {
   name        = "venket_ssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-09db8688d039eb48a"
